6b03ec7d5e8d4544506d9e932ccce48e83d68243
Alexander Voigt <Alexander.Voigt@desy.de>
2017-01-01 22:58:13 +0100

    delete old plots

--- Makefile
+++ Makefile
@@ -1,24 +1,19 @@
 # http://www.gnu.org/software/make/manual/make.html
 
 OUTFILENAME := talk.pdf
-PLOTS       := plots/PlotScale-in-FH_new_low.pdf \
-               plots/PlotScale-in-FH_new_low-selected.pdf \
-               plots/PlotScale-in-FH_new_low-selected-high.pdf \
-               plots/PlotScale-in-FH_new_low-notower.pdf \
-               plots/PlotScale-in-FH_new_low-fhsusyhd.pdf \
-               plots/PlotScale-in-FH_new_low-fhsusyhd-error.pdf \
-               plots/PlotXt.pdf \
-               plots/PlotXt-selected.pdf \
-               plots/Xt_TB5_MS1000.pdf \
-               plots/Xt_TB5_MS10000.pdf \
-               plots/Xt_TB20_MS2000.pdf \
-               plots/scale_MSSM_Xt-0.pdf \
-               plots/FlexibleEFTHiggs/Mh_MS_TB-5_Xt-0.pdf \
-               plots/FlexibleEFTHiggs/Mh_relative_MS_TB-5_Xt-0.pdf \
-               plots/FlexibleEFTHiggs/Mh_MS_TB-5_Xt--2.pdf \
-               plots/FlexibleEFTHiggs/Mh_relative_MS_TB-5_Xt--2.pdf \
-               plots/FlexibleEFTHiggs/Mh_Xt_TB-5_MS-2000.pdf \
-               plots/FlexibleEFTHiggs/Mh_relative_Xt_TB-5_MS-2000.pdf
+PLOTS       := \
+		plots/FlexibleEFTHiggs/DMh_tower-1L_MS_TB-5_Xt-0.pdf \
+		plots/FlexibleEFTHiggs/DMh_tower-1L_MS_TB-5_Xt--2.pdf \
+		plots/FlexibleEFTHiggs/DMh_tower-1L_Xt_TB-5_MS-2000.pdf \
+		plots/FlexibleEFTHiggs/DMh_tower-2L_MS_TB-5_Xt-0.pdf \
+		plots/FlexibleEFTHiggs/DMh_tower-2L_MS_TB-5_Xt--2.pdf \
+		plots/FlexibleEFTHiggs/DMh_tower-2L_Xt_TB-5_MS-2000.pdf \
+		plots/FlexibleEFTHiggs/Mh_MS_TB-5_Xt-0.pdf \
+		plots/FlexibleEFTHiggs/Mh_MS_TB-5_Xt--2.pdf \
+		plots/FlexibleEFTHiggs/Mh_relative_MS_TB-5_Xt-0.pdf \
+		plots/FlexibleEFTHiggs/Mh_relative_MS_TB-5_Xt--2.pdf \
+		plots/FlexibleEFTHiggs/Mh_relative_Xt_TB-5_MS-2000.pdf \
+		plots/FlexibleEFTHiggs/Mh_Xt_TB-5_MS-2000.pdf
 TEXDIRS     := $(PLOTSDIR)
 BIBTEX      := bibtex
 
@@ -26,43 +21,7 @@ BIBTEX      := bibtex
 
 all: $(OUTFILENAME)
 
-plots/PlotScale-in-FH_new_low.pdf: plots/plot-mh-ms.gnuplot plots/PlotScale.in.FH_new_low
-	cd plots && gnuplot $(shell basename $<)
-
-plots/PlotScale-in-FH_new_low-selected.pdf: plots/plot-mh-ms-selected.gnuplot plots/PlotScale.in.FH_new_low
-	cd plots && gnuplot $(shell basename $<)
-
-plots/PlotScale-in-FH_new_low-selected-high.pdf: plots/plot-mh-ms-selected-high.gnuplot plots/scale_high_TB5.dat
-	cd plots && gnuplot $(shell basename $<)
-
-plots/PlotScale-in-FH_new_low-notower.pdf: plots/plot-mh-ms-notower.gnuplot plots/PlotScale.in.FH_new_low
-	cd plots && gnuplot $(shell basename $<)
-
-plots/PlotScale-in-FH_new_low-fhsusyhd.pdf: plots/plot-mh-ms-fhsusyhd.gnuplot plots/PlotScale.in.FH_new_low
-	cd plots && gnuplot $(shell basename $<)
-
-plots/PlotScale-in-FH_new_low-fhsusyhd-error.pdf: plots/plot-mh-ms-fhsusyhd-error.gnuplot plots/PlotScale.in.FH_new_low
-	cd plots && gnuplot $(shell basename $<)
-
-plots/PlotXt.pdf: plots/plot-mh-xt.gnuplot plots/PlotXt.in
-	cd plots && gnuplot $(shell basename $<)
-
-plots/PlotXt-selected.pdf: plots/plot-mh-xt-selected.gnuplot plots/PlotXt.in
-	cd plots && gnuplot $(shell basename $<)
-
-plots/Xt_*.pdf: plots/plot-mh-xt-alex.gnuplot plots/Xt_*.dat
-	cd plots && gnuplot $(shell basename $<)
-
-plots/scale_MSSM_Xt-0.pdf: plots/scale_MSSM.gnuplot plots/scale_MSSM.dat
-	cd plots && gnuplot $(shell basename $<)
-
-plots/FlexibleEFTHiggs/Mh_MS_TB-5_Xt-0.pdf \
-plots/FlexibleEFTHiggs/Mh_relative_MS_TB-5_Xt-0.pdf \
-plots/FlexibleEFTHiggs/Mh_MS_TB-5_Xt--2.pdf \
-plots/FlexibleEFTHiggs/Mh_relative_MS_TB-5_Xt--2.pdf \
-plots/FlexibleEFTHiggs/Mh_Xt_TB-5_MS-2000.pdf \
-plots/FlexibleEFTHiggs/Mh_relative_Xt_TB-5_MS-2000.pdf \
-: plots/FlexibleEFTHiggs/*.dat plots/FlexibleEFTHiggs/*.sh plots/FlexibleEFTHiggs/*.gnuplot
+$(PLOTS): plots/FlexibleEFTHiggs/*.dat plots/FlexibleEFTHiggs/*.sh plots/FlexibleEFTHiggs/*.gnuplot
 	cd plots/FlexibleEFTHiggs && ./plot.sh
 
 %.pdf: %.tex $(PLOTS)
--- plots/FlexibleEFTHiggs/plot.sh
+++ plots/FlexibleEFTHiggs/plot.sh
@@ -6,16 +6,7 @@ plot() {
     local y1=$3
     local x2=$4
     local y2=$5
-    local label=$6
-    local log=$7
-    local scaling=$8
-    local xlabel=$9
 
-    local setlabel=
-    local logscale=
-
-    [ -n "$label" ] && setlabel="set label 100 \"${label}\" at first ${label}, graph -0.055 center"
-    [ -n "$log" ] && logscale="set logscale x"
     [ -z "$scaling" ] && scaling=1
 
 cat <<EOF | gnuplot
@@ -27,9 +18,12 @@ Mt = 0.17334
 ${setlabel}
 ${logscale}
 set grid
-set xlabel '${xlabel}'
+set xlabel "${xlabel}"
 
+min(x,y) = x < y ? x : y
+max(x,y) = x < y ? y : x
 ran(left,right,data) = (data < left || data > right ? 1/0 : data)
+dQ(Mhmean,Mhmax,Mhmin) = max(abs(Mhmean-Mhmax), abs(Mhmean-Mhmin))
 
 # MSSMtower
 # 1: MS
@@ -49,7 +43,7 @@ ran(left,right,data) = (data < left || data > right ? 1/0 : data)
 #15: g3^SM(MS)
 #16: g3^SM(Mt)
 
-set key box bottom right width -1 height 0.5 opaque font ",10"
+set key box bottom right width -1 height 0.5 opaque
 set ylabel 'M_h / GeV'
 set output dir.'/Mh_${s}.pdf'
 dataTower1L = dir.'/MSSMtower1L_${s}.dat'
@@ -69,6 +63,7 @@ plot [${x1}] [${y1}] \
      dataSGs   u (\$1/${scaling}):(\$10-\$11):(\$10+\$11) t '' w filledcurves ls 19 fs transparent solid 0.3
 
 set output dir.'/Mh_relative_${s}.pdf'
+set key font ",10"
 set ylabel '(M_h - M_h^{FlexibleEFTHiggs/MSSM 2L}) / GeV'
 set key box top left width -1 height 0.5 opaque
 
@@ -85,14 +80,44 @@ plot [${x2}] [${y2}] \
      data u (\$33/${scaling}):(\$40-\$41-\$18):(\$40+\$41-\$18) t '' w filledcurves ls 18 fs transparent solid 0.3, \
      data u (\$33/${scaling}):(\$42-\$18) t 'SUSYHD 1.0.2' w lines ls 9, \
      data u (\$33/${scaling}):(\$42-\$43-\$18):(\$42+\$43-\$18) t '' w filledcurves ls 19 fs transparent solid 0.3
+
+###### DMh ######
+
+set style line 1 lt 4 lw 0 dt 1 lc rgb '#00FFFF'
+set style line 2 lt 4 lw 0 dt 1 lc rgb '#FF00FF'
+set style line 3 lt 4 lw 0 dt 1 lc rgb '#FFFF00'
+set key box bottom right width 1 height 0.5 font ",12"
+
+set output dir.'/DMh_tower-1L_${s}.pdf'
+set ylabel "{/Symbol D} M_h / GeV"
+data = dataTower1L
+
+plot [${x1}] [-6:6] \
+     data u (\$1/${scaling}):(-\$4):(\$4) t "{/Symbol D}M_h^{(y_t 0L vs. 1L)}" \
+     w filledcurves ls 1 fs transparent solid 0.5, \
+     data u (\$1/${scaling}):(-dQ(\$2,\$5,\$6)):(dQ(\$2,\$5,\$6)) t "{/Symbol D}M_h^{(Q_{match})}" \
+     w filledcurves ls 2 fs transparent solid 0.5, \
+     data u (\$1/${scaling}):(-dQ(\$2,\$7,\$8)):(dQ(\$2,\$7,\$8)) t "{/Symbol D}M_h^{(Q_{pole})}" \
+     w filledcurves ls 3 fs transparent solid 0.5
+
+set output dir.'/DMh_tower-2L_${s}.pdf'
+set ylabel "{/Symbol D} M_h / GeV"
+data = dataTower2L
+
+plot [${x1}] [-6:6] \
+     data u (\$1/${scaling}):(-\$4):(\$4) t "{/Symbol D}M_h^{(y_t 1L vs. 2L)}" \
+     w filledcurves ls 1 fs transparent solid 0.5, \
+     data u (\$1/${scaling}):(-dQ(\$2,\$5,\$6)):(dQ(\$2,\$5,\$6)) t "{/Symbol D}M_h^{(Q_{match})}" \
+     w filledcurves ls 2 fs transparent solid 0.5, \
+     data u (\$1/${scaling}):(-dQ(\$2,\$7,\$8)):(dQ(\$2,\$7,\$8)) t "{/Symbol D}M_h^{(Q_{pole})}" \
+     w filledcurves ls 3 fs transparent solid 0.5
 EOF
 
 }
 
 scenario="
- MS_TB-5_Xt-0,Mt:,80:140,Mt:,-4:10,0.2,log,1000,M_S/TeV
- MS_TB-5_Xt--2,0.3:,95:140,0.3:,-4:10,0.3,log,1000,M_S/TeV
- Xt_TB-5_MS-2000,-3.5:3.5,:,-3.5:3.5,-4:10,,,,X_t/M_S
+ MS_TB-5_Xt-0,Mt:,80:140,Mt:,-4:10,0.2
+ MS_TB-5_Xt--2,0.3:,95:140,0.3:,-4:10,0.3
 "
 
 for s in $scenario ; do
@@ -102,7 +127,29 @@ for s in $scenario ; do
     x2=$(echo "$s" | cut -d',' -f 4)
     y2=$(echo "$s" | cut -d',' -f 5)
     label=$(echo "$s" | cut -d',' -f 6)
-    log=$(echo "$s" | cut -d',' -f 7)
-    scaling=$(echo "$s" | cut -d',' -f 8)
-    plot $scen $x1 $y1 $x2 $y2 $label $log $scaling
+    setlabel=
+    [ -n "$label" ] && setlabel="set label 100 \"${label}\" at first ${label}, graph -0.055 center"
+    logscale="set logscale x"
+    scaling=1000
+    xlabel="M_S / TeV"
+    plot "$scen" "$x1" "$y1" "$x2" "$y2" "$label"
+done
+
+scenario="
+ Xt_TB-5_MS-2000,-3.5:3.5,:,-3.5:3.5,-4:10
+"
+
+for s in $scenario ; do
+    scen=$(echo "$s" | cut -d',' -f 1)
+    x1=$(echo "$s" | cut -d',' -f 2)
+    y1=$(echo "$s" | cut -d',' -f 3)
+    x2=$(echo "$s" | cut -d',' -f 4)
+    y2=$(echo "$s" | cut -d',' -f 5)
+    label=$(echo "$s" | cut -d',' -f 6)
+    setlabel=
+    [ -n "$label" ] && setlabel="set label 100 \"${label}\" at first ${label}, graph -0.055 center"
+    logscale=
+    scaling=
+    xlabel="X_t / M_S"
+    plot "$scen" "$x1" "$y1" "$x2" "$y2"
 done
