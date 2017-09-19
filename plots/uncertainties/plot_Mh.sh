0f78369a4fbf5b7285e5504dec120dd3b949af33
Alexander Voigt <Alexander.Voigt@physik.rwth-aachen.de>
2017-09-19 15:03:32 +0200

    plot uncertainty bands

--- plots/uncertainties/plot_Mh.sh
+++ plots/uncertainties/plot_Mh.sh
@@ -17,18 +17,23 @@ outfile = directory + r'Mh_MS_TB-5_Xt-0.pdf'
 try:
     dataMSSMEFTHiggs = np.genfromtxt(directory + r'MSSMEFTHiggs_MS_TB-5_Xt-0..dat')
     dataMSSM         = np.genfromtxt(directory + r'scan_FeynHiggs-2.13.0beta_Mh_MS_TB-5_Xt-0.dat')
-    dataSUSYHD       = np.genfromtxt(directory + r'HSSUSY_MS_TB-5_Xt-0..dat')
+    dataSUSYHD       = np.genfromtxt(directory + r'SUSYHD_MS_TB-5_Xt-0..dat')
+    dataHSSUSY       = np.genfromtxt(directory + r'HSSUSY_MS_TB-5_Xt-0..dat')
 except:
     print "Error: could not load numerical data from file"
     exit
 
 xMSSMEFTHiggs = dataMSSMEFTHiggs[:,0]
 xMSSM         = dataMSSM[:,0]
-xSUSYHD       = dataSUSYHD[:,0]
+xSUSYHD       = dataHSSUSY[:,0]
 
 yMSSMEFTHiggs = dataMSSMEFTHiggs[:,1]
 yMSSM         = dataMSSM[:,1]
-ySUSYHD       = dataSUSYHD[:,1]
+ySUSYHD       = dataHSSUSY[:,1]
+
+dMSSMEFTHiggs = dataMSSMEFTHiggs[:,2]
+dMSSM         = dataMSSM[:,2]
+dSUSYHD       = dataSUSYHD[:,2]
 
 plt.rc('text', usetex=True)
 plt.rc('font', family='serif', weight='normal')
@@ -47,10 +52,17 @@ plt.xscale('log')
 plt.xlabel(r'$M_S\,/\,\mathrm{GeV}$')
 plt.ylabel(r'$M_h\,/\,\mathrm{GeV}$')
 plt.xlim([100,10000])
+plt.ylim([75,130])
 
 hMSSM, = plt.plot(xMSSM        , yMSSM        , 'b--', linewidth=1.2, label=r'2L fixed order')
 hmix,  = plt.plot(xMSSMEFTHiggs, yMSSMEFTHiggs, 'r-' , linewidth=1.2, label=r'1L FlexibleEFTHiggs')
 hEFT,  = plt.plot(xSUSYHD      , ySUSYHD      , 'g-.', linewidth=1.2, dashes=(3,2,1,2), label=r'2L EFT')
+plt.fill_between(xMSSM, yMSSM - dMSSM, yMSSM + dMSSM,
+                 color='blue', alpha=0.3, interpolate=True, linewidth=0.0)
+plt.fill_between(xSUSYHD, ySUSYHD - dSUSYHD, ySUSYHD + dSUSYHD,
+                 color='green', alpha=0.3, interpolate=True, linewidth=0.0)
+# plt.fill_between(xMSSMEFTHiggs, yMSSMEFTHiggs - dMSSMEFTHiggs, yMSSMEFTHiggs + dMSSMEFTHiggs,
+#                  color='red', alpha=0.3, interpolate=True, linewidth=0.0)
 
 leg = plt.legend(handles = [hMSSM, hEFT, hmix],
                  loc='lower right', fontsize=10, fancybox=None, framealpha=None)
