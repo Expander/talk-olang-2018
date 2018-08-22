ef3c3c18f2d134ad3e1cce439f334ed0b3fcb1d0
Alexander Voigt <Alexander.Voigt@physik.rwth-aachen.de>
2018-08-22 12:39:56 +0200

    remove unused files

--- Feynman/makeall.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#for f in *.mp; do mpost $f; done
-
-mpfiles=`find . -type f -name '*.mp' -not -name 'feynmp.mp' -printf "%f\n"`
-echo "Compile files with MetaPost:"
-echo $mpfiles
-for f in $mpfiles; do mpost $f; done
